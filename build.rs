c6e18130296f54bc101e096c486eb934e34e88a5
Nick Tobey <nicktobey@nicktobey.me>
2021-06-01 19:40:18 -0700

    Test cross-platform support

--- build.rs
+++ build.rs
@@ -1,25 +1,62 @@
 extern crate bindgen;
+extern crate vcpkg;
 
 use std::env;
 use std::path::PathBuf;
+use std::collections::HashSet;
 
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
 
+// A workaround for https://github.com/rust-lang/rust-bindgen/issues/687
+// This is necessary in order to bind anything that includes glibc's version of math.h
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
 
 fn main() {
 
-    println!("cargo:rustc-link-search=./lib/PDCurses-3.9/wincon");
-    println!("cargo:rustc-link-lib=pdcurses");
-    println!("cargo:rustc-link-search=./lib/SDL2/Release");
-    println!("cargo:rustc-link-lib=sdl2");
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+            "IPPORT_RESERVED".into(),   
+        ]
+        .into_iter()
+        .collect(),
+    );
 
     println!("cargo:rerun-if-changed=wrapper.h");
 
+    vcpkg::Config::new()
+        .emit_includes(true)
+        .find_package("ncurses")
+        .unwrap();
+
+    vcpkg::Config::new()
+        .emit_includes(true)
+        .find_package("sdl2")
+        .unwrap();
+
     let bindings = bindgen::Builder::default().header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(ignored_macros))
+        .rustfmt_bindings(true)
+        .clang_arg("-Itarget/vcpkg/installed/x64-linux/include/")
         .generate().expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     bindings.write_to_file(out_path.join("bindings.rs"))
             .expect("Couldn't write to bindings!");
+
 }
--- wrapper.h
+++ wrapper.h
@@ -1,2 +1,6 @@
-#include "lib/PDCurses-3.9/curses.h"
-#include "lib/SDL2-2.0.12/include/SDL.h"
+// #include "lib/PDCurses-3.9/curses.h"
+// #include "lib/SDL2-2.0.12/include/SDL.h"
+// #include "target/vcpkg/installed/x64-linux/include/SDL2/SDL.h"
+// #include "target/vcpkg/installed/x64-linux/include/ncurses/ncurses.h"
+#include <ncurses/ncurses.h>
+#include <SDL2/SDL.h>
